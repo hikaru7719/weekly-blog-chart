63528e1e7a932eed86799a828a77bcc339adf3f6
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-03-25 23:49:27 +0900

    expressを追加

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import next from "next";
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.get("*", (req: express.Request, res: express.Response) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
